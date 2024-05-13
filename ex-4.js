07f454f86fd68f6000d185ccf24f24363d49aa74
Moth <kampanee.moth@gmail.com>
2024-05-13 14:51:31 +0700

    feat: add ex-1 to ex-7 .js

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,12 @@ const employees = [
 ];
 
 // Start coding here
+let alicia = {
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+};
+let kody = { name: "Kody", age: 19, hobbies: ["Computer games", "Wakeboard"] };
+employees.push(alicia, kody);
+
+console.log(employees);
--- ex-7.js
+++ ex-7.js
@@ -46,3 +46,21 @@ let isArray;
 let creditCardTypeOfBlindermann;
 let productQuantityOfJoannet;
 let totalPurchaseOfDary;
+
+isArray = Array.isArray(orders);
+console.log(isArray);
+
+creditCardTypeOfBlindermann = orders[2].creditCardType;
+console.log(creditCardTypeOfBlindermann);
+
+orders[2].creditCardType = "visa";
+console.log(orders[2].creditCardType);
+
+productQuantityOfJoannet = orders[3].productQuantity;
+console.log(productQuantityOfJoannet);
+
+totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+console.log(totalPurchaseOfDary);
+
+orders.shift();
+console.log(orders);
